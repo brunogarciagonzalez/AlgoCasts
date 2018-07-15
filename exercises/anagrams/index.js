c3840d4d668339554409cdf355247cea58ac4b59
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-07-15 17:42:31 -0400

    complete

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function charMap(string) {
+  let outputMap = {};
+
+  for (char of string) {
+    if (!outputMap[char]) {
+      outputMap[char] = 1;
+    } else {
+      outputMap[char] += 1;
+    }
+  }
+
+  return outputMap;
+}
+
+function anagrams(stringA, stringB) {
+  // .replace(/[^\w]/g, "").toLowerCase()
+  let moddedA = stringA.replace(/[\W_]/g, "").toLowerCase();
+  let moddedB = stringB.replace(/[\W_]/g, "").toLowerCase();
+  let charMapA = charMap(moddedA);
+  let charMapB = charMap(moddedB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  for (let key in charMapA) {
+    if (charMapA[key] !== charMapB[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
