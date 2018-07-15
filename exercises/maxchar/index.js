b18a20931d93ce1f98a4a0879fcb17b2acffd39e
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-07-15 16:21:33 -0400

    complete

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (character of str) {
+    // if (charMap[character]){
+    //   charMap[character]+=1
+    // } else
+    // charMap[character]=1
+
+    charMap[character] = charMap[character] + 1 || 1;
+  }
+
+  for (let key in charMap) {
+    if (charMap[key] > max) {
+      max = charMap[key];
+      maxChar = key;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
