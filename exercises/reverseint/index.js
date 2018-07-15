d97b5a8154d2d3a76bde809496d654a1a20b0c25
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-07-15 15:54:19 -0400

    complete

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // if neg stay neg
+  let negBool = n < 0;
+  if (negBool) {
+    return (
+      -1 *
+      parseInt(
+        n
+          .toString()
+          .split("-")[1]
+          .split("")
+          .reverse()
+          .join("")
+      )
+    );
+  } else {
+    return parseInt(
+      n
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    );
+  }
+}
 
 module.exports = reverseInt;
